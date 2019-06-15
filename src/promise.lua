02f79f17b137ad7cdf66bdfc0dddb181c7e8581f
pyericz <pyericz@yahoo.com>
2019-06-15 23:36:28 +0800

    support Promise.resolve, Promise.reject, Promise.race and Promise.all.

--- src/promise.lua
+++ src/promise.lua
@@ -2,12 +2,15 @@ local PENDING = 'pending'
 local FULFILLED = 'fulfilled'
 local REJECTED = 'rejected'
 
+local function noop() end
+
 local function isThenable(x)
     return type(x) == 'table' and x.thenCall ~= nil
 end
 
-local Promise = {}
+local promise = {}
 
+local newPromise
 local resolve
 local promiseOnFulfilled
 local promiseOnRejected
@@ -16,7 +19,7 @@ local function isPromise(x)
     if type(x) ~= 'table' then return false end
     local mt = getmetatable(x)
     while mt ~= nil do
-        if mt == Promise then
+        if mt == promise then
             return true
         end
         mt = getmetatable(mt)
@@ -52,92 +55,92 @@ local function execRejected(thenInfo, reason)
     end
 end
 
-promiseOnFulfilled = function (promise, value)
-    if promise.state == PENDING then
-        promise.value = value
-        promise.reason = nil
-        promise.state = FULFILLED
+promiseOnFulfilled = function (p, value)
+    if p.state == PENDING then
+        p.value = value
+        p.reason = nil
+        p.state = FULFILLED
     end
-    for _,n in ipairs(promise.thenInfoList) do
+    for _,n in ipairs(p.thenInfoList) do
         execFulfilled(n, value)
     end
 end
 
-promiseOnRejected = function (promise, reason)
-    if promise.state == PENDING then
-        promise.value = nil
-        promise.reason = reason
-        promise.state = REJECTED
+promiseOnRejected = function (p, reason)
+    if p.state == PENDING then
+        p.value = nil
+        p.reason = reason
+        p.state = REJECTED
     end
-    for _,n in ipairs(promise.thenInfoList) do
+    for _,n in ipairs(p.thenInfoList) do
         execRejected(n, reason)
     end
 end
 
 
-local function resolveThenable(promise, x)
+local function resolveThenable(p, x)
     local thenCall = x.thenCall
     if type(thenCall) == 'function' then
         local isCalled = false
         local function resolvePromise(y)
             if isCalled then return end
             isCalled = true
-            resolve(promise, y)
+            resolve(p, y)
         end
         local function rejectPromise(r)
             if isCalled then return end
             isCalled = true
-            promiseOnRejected(promise, r)
+            promiseOnRejected(p, r)
         end
         local success, err = pcall(thenCall, x, resolvePromise, rejectPromise)
         if not success then
             if not isCalled then
-                promiseOnRejected(promise, err)
+                promiseOnRejected(p, err)
             end
         end
     else
-        promiseOnFulfilled(promise, x)
+        promiseOnFulfilled(p, x)
     end
 end
 
 --[[
-    define promise resolution procedure
+    define p resolution procedure
 --]]
-resolve = function (promise, x)
-    if promise == x then
-        promiseOnRejected(promise, 'TypeError')
+resolve = function (p, x)
+    if p == x then
+        promiseOnRejected(p, 'TypeError')
         return
     end
     if isPromise(x) then
         if x.state == PENDING then
-            promise.state = PENDING
+            p.state = PENDING
         end
-        resolveThenable(promise, x)
+        resolveThenable(p, x)
     elseif isThenable(x) then
-        resolveThenable(promise, x)
+        resolveThenable(p, x)
     else
-        promiseOnFulfilled(promise, x)
+        promiseOnFulfilled(p, x)
     end
 end
 
 
-function Promise:new()
-    local promise = {}
-    setmetatable(promise, self)
+function promise:new()
+    local p = {}
+    setmetatable(p, self)
     self.__index = self
-    promise.thenInfoList = {}
-    promise.state = PENDING
-    promise.value = nil
-    promise.reason = nil
+    p.thenInfoList = {}
+    p.state = PENDING
+    p.value = nil
+    p.reason = nil
 
-    return promise
+    return p
 end
 
-function Promise:thenCall(onFulfilled, onRejected)
-    local promise = Promise:new()
+function promise:thenCall(onFulfilled, onRejected)
+    local p = newPromise(noop)
 
     local thenInfo = {
-        promise = promise,
+        promise = p,
     }
 
     if type(onFulfilled) == 'function' then
@@ -156,14 +159,14 @@ function Promise:thenCall(onFulfilled, onRejected)
 
     table.insert(self.thenInfoList, thenInfo)
 
-    return promise
+    return p
 end
 
-function Promise:catch(onRejected)
+function promise:catch(onRejected)
     return self:thenCall(nil, onRejected)
 end
 
-function Promise:finally(func)
+function promise:finally(func)
     return self:thenCall(
         function ()
             func()
@@ -174,8 +177,8 @@ function Promise:finally(func)
     )
 end
 
-return function (func)
-    local obj = Promise:new()
+newPromise = function (func)
+    local obj = promise:new()
     local isCalled = false
     local function onFulfilled(value)
         if isCalled then return end
@@ -194,3 +197,104 @@ return function (func)
     end
     return obj
 end
+
+local Promise = {}
+setmetatable(Promise, {
+    __call = function (_, func)
+        return newPromise(func)
+    end
+})
+
+Promise.new = newPromise
+
+local function newPromiseFromValue(value)
+    local p = newPromise(noop)
+    p.state = FULFILLED
+    p.value = value
+    p.reason = nil
+    return p
+end
+
+function Promise.resolve(value)
+    if isPromise(value) then return value end
+    if isThenable(value) then
+        local thenCall = value.thenCall
+        if type(thenCall) == 'function' then
+            return newPromise(function(onFulfilled, onRejected)
+                value:thenCall(onFulfilled, onRejected)
+            end)
+        else
+            return newPromise(function(_, onRejected)
+                onRejected('TypeError')
+            end)
+        end
+    end
+    return newPromiseFromValue(value)
+end
+
+
+function Promise.reject(value)
+    return newPromise(function(_, onRejected)
+        onRejected(value)
+    end)
+end
+
+function Promise.race(values)
+    assert(type(values) == 'table', 'Promise.race needs an table')
+    assert(next(values) ~= nil, 'An empty table')
+    return newPromise(function(onFulfilled, onRejected)
+        for _, value in pairs(values) do
+            Promise.resolve(value):thenCall(onFulfilled, onRejected)
+        end
+    end)
+end
+
+function Promise.all(array)
+    assert(type(array) == 'table', 'Promise.all needs an array')
+    local args = {}
+    for i=1, #array do
+        args[i] = array[i]
+    end
+
+    return newPromise(function (onFulfilled, onRejected)
+        if #args == 0 then return onFulfilled({}) end
+        local remaining = #args
+        local function res(i, val)
+            if val then
+                if isPromise(val) then
+                    if val.state == FULFILLED then
+                        return res(i, val.value)
+                    end
+                    if val.state == REJECTED then
+                        onRejected(val.reason)
+                    end
+                    val:thenCall(function (v)
+                        res(i, v)
+                    end, onRejected)
+                    return
+                elseif isThenable(val) then
+                    local thenCall = val.thenCall
+                    if type(thenCall) == 'function' then
+                        local p = newPromise(function(r, rj)
+                            val:thenCall(r, rj)
+                        end)
+                        p:thenCall(function (v)
+                            res(i, v)
+                        end, onRejected)
+                        return
+                    end
+                end
+            end
+            args[i] = val
+            remaining = remaining - 1
+            if remaining == 0 then
+                onFulfilled(args)
+            end
+        end
+        for i=1, #args do
+            res(i, args[i])
+        end
+    end)
+end
+
+return Promise
